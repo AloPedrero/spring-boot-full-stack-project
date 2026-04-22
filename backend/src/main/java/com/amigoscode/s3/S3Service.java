339e559c2b480da10d6e99e1e40fae64613be6c5
AloPedrero <pedreromartinezalonso@gmail.com>
2026-04-22 13:37:05 -0700

    feat: Created the get object service

--- backend/src/main/java/com/amigoscode/s3/S3Service.java
+++ backend/src/main/java/com/amigoscode/s3/S3Service.java
@@ -1,9 +1,14 @@
 package com.amigoscode.s3;
 
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
 
 @Service
 public class S3Service {
@@ -19,5 +24,22 @@ public class S3Service {
             .key(key)
             .build();
         s3Client.putObject(objectRequest, RequestBody.fromBytes(file));
+    }
+
+    public byte[] getObject(String bucketName, String key) {
+            GetObjectRequest request = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+
+            s3Client.getObject(request);
+        ResponseInputStream<GetObjectResponse> response = s3Client.getObject(request);
+
+        try {
+            return response.readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
